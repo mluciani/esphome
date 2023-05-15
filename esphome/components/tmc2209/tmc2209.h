213b8b24d2c47241807aee9730532f83a2ea8519
lumascet <lukas.schroeer@gmx.at>
2023-05-15 12:00:06 +0200

    fixed syntax errors

--- esphome/components/tmc2209/tmc2209.h
+++ esphome/components/tmc2209/tmc2209.h
@@ -11,7 +11,7 @@ namespace tmc {
 
 class TMC2209 : public stepper::Stepper, public Component, public uart::UARTDevice {
  public:
-  TMC2209(GPIOPin *step_pin, GPIOPin *dir_pin, bool reverse_direction, *float r_shunt, *uint8_t tmc_address)
+  TMC2209(GPIOPin *step_pin, GPIOPin *dir_pin, bool reverse_direction, float *r_shunt, uint8_t *tmc_address)
       : step_pin_(step_pin), dir_pin_(dir_pin), reverse_direction_(reverse_direction), r_shunt_(r_shunt), tmc_address_(tmc_address) {}
 
   void set_sleep_pin(GPIOPin *sleep_pin) { this->sleep_pin_ = sleep_pin; }
@@ -62,7 +62,7 @@ template<typename... Ts> class TMC2209SetupAction : public Action<Ts...>, public
       driver.rms_current(static_cast<int>(this->current_.value(x...) * 1000.0));
     }
     if (this->tmc_address_.has_value()) {
-      ESP_LOGW("tmc2209", "tmc_address %.3f", this->tmc_address_.value(x...));
+      ESP_LOGW("tmc2209", "tmc_address %d", this->tmc_address_.value(x...));
     }
     if (this->r_shunt_.has_value()) {
       ESP_LOGW("tmc2209", "r_shunt_ %.3f", this->r_shunt_.value(x...));
