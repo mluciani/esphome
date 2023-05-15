7d4e4fb81b165d5fd1c80e881a713b7c04a8892b
lumascet <lukas.schroeer@gmx.at>
2023-05-15 12:03:49 +0200

    removed pointer

--- esphome/components/tmc2209/tmc2209.h
+++ esphome/components/tmc2209/tmc2209.h
@@ -11,7 +11,7 @@ namespace tmc {
 
 class TMC2209 : public stepper::Stepper, public Component, public uart::UARTDevice {
  public:
-  TMC2209(GPIOPin *step_pin, GPIOPin *dir_pin, bool reverse_direction, float *r_shunt, uint8_t *tmc_address)
+  TMC2209(GPIOPin *step_pin, GPIOPin *dir_pin, bool reverse_direction, float r_shunt, uint8_t tmc_address)
       : step_pin_(step_pin), dir_pin_(dir_pin), reverse_direction_(reverse_direction), r_shunt_(r_shunt), tmc_address_(tmc_address) {}
 
   void set_sleep_pin(GPIOPin *sleep_pin) { this->sleep_pin_ = sleep_pin; }
@@ -30,8 +30,8 @@ class TMC2209 : public stepper::Stepper, public Component, public uart::UARTDevi
   GPIOPin *sleep_pin_{nullptr};
   bool sleep_pin_state_;
   HighFrequencyLoopRequester high_freq_;
-  double *r_shunt_;
-  uint8_t *tmc_address_;
+  double r_shunt_;
+  uint8_t tmc_address_;
 };
 
 template<typename... Ts> class TMC2209SetupAction : public Action<Ts...>, public Parented<TMC2209> {
@@ -61,11 +61,11 @@ template<typename... Ts> class TMC2209SetupAction : public Action<Ts...>, public
       ESP_LOGW("tmc2209", "current %.3f", this->current_.value(x...));
       driver.rms_current(static_cast<int>(this->current_.value(x...) * 1000.0));
     }
-    if (this->tmc_address_.has_value()) {
-      ESP_LOGW("tmc2209", "tmc_address %d", this->tmc_address_.value(x...));
+    if (tmc_address_.has_value()) {
+      ESP_LOGW("tmc2209", "tmc_address %d", tmc_address_.value(x...));
     }
-    if (this->r_shunt_.has_value()) {
-      ESP_LOGW("tmc2209", "r_shunt_ %.3f", this->r_shunt_.value(x...));
+    if (r_shunt_.has_value()) {
+      ESP_LOGW("tmc2209", "r_shunt_ %.3f", r_shunt_.value(x...));
     }
   }
 };
